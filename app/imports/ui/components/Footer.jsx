3d668f5fd1cf599127a48cdb79b043f24a7e67c9
Benny <76574420+bennytrieu@users.noreply.github.com>
2023-02-07 17:17:38 -1000

    commented out footer information and Changed texts on landing

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -5,20 +5,20 @@ import { Col, Container } from 'react-bootstrap';
 const Footer = () => (
   <footer className="mt-auto py-3 bg-light">
     <Container>
-      <Col className="text-center">
-        Department of Information and Computer Sciences
-        {' '}
-        <br />
-        University of Hawaii
-        <br />
-        Honolulu, HI 96822
-        {' '}
-        <br />
-        <a href="http://ics-software-engineering.github.io/meteor-application-template-react">
-          Template Home
-          Page
-        </a>
-      </Col>
+      {/*<Col className="text-center">*/}
+      {/*  Department of Information and Computer Sciences*/}
+      {/*  {' '}*/}
+      {/*  <br />*/}
+      {/*  University of Hawaii*/}
+      {/*  <br />*/}
+      {/*  Honolulu, HI 96822*/}
+      {/*  {' '}*/}
+      {/*  <br />*/}
+      {/*  <a href="http://ics-software-engineering.github.io/meteor-application-template-react">*/}
+      {/*    Template Home*/}
+      {/*    Page*/}
+      {/*  </a>*/}
+      {/*</Col>*/}
     </Container>
   </footer>
 );
--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -5,13 +5,13 @@ import { Col, Container, Image, Row } from 'react-bootstrap';
 const Landing = () => (
   <Container id="landing-page" fluid className="py-3">
     <Row className="align-middle text-center">
-      <Col xs={4}>
-        <Image roundedCircle src="/images/meteor-logo.png" width="150px" />
-      </Col>
+      {/* <Col xs={4}> */}
+      {/*  <Image roundedCircle src="/images/meteor-logo.png" width="150px" /> */}
+      {/* </Col> */}
 
-      <Col xs={8} className="d-flex flex-column justify-content-center">
-        <h1>Welcome to this template</h1>
-        <p>Now get to work and modify asdasdas</p>
+      <Col className="d-flex flex-column justify-content-center">
+        <h1>Passbots</h1>
+        <p>Where we store your passwords</p>
       </Col>
 
     </Row>
