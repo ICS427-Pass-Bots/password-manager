1af4732b07b2ff741dc645474bcbe2ed7f768865
John Suelen <jsuelen@hawaii.edu>
2023-02-21 21:38:34 -1000

    show and hide password rough

--- app/imports/ui/components/PasswordItem.jsx
+++ app/imports/ui/components/PasswordItem.jsx
@@ -1,25 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
+/* const toggleCell = () => {
+  const cell = document.getElementById('hidden-cell');
+  if (cell.classList.contains('hidden')) {
+    cell.classList.remove('hidden');
+  } else {
+    cell.classList.add('hidden');
+  }
+}; */
+
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
-const PasswordItem = ({ password }) => (
-  <tr>
-    <td>{password.website}</td>
-    <td>{password.username}</td>
-    <td>{password.password}</td>
-    <td>
-      <Link to={`/edit/${password._id}`}>Edit</Link>
-    </td>
-  </tr>
-);
+const PasswordItem = ({ password }) => {
+  const [show, setShow] = useState(true);
+  return (
+    <tr>
+      <td>{password.website}</td>
+      <td>{password.username}</td>
+      {show ? <td>{password.password}</td> : null}
+      <td>
+        <button type="button" onClick={() => setShow(!show)}>
+          Toggle Password
+        </button>
+      </td>
+      <td>
+        <Link to={`/edit/${password._id}`}>Edit</Link>
+      </td>
+    </tr>
+  );
+};
 
 // Require a document to be passed to this component.
 PasswordItem.propTypes = {
   password: PropTypes.shape({
     website: PropTypes.string,
-    username: PropTypes.number,
+    username: PropTypes.string,
     password: PropTypes.string,
+    buttonID: PropTypes.number,
     _id: PropTypes.string,
   }).isRequired,
 };
--- app/imports/ui/pages/ListPassword.jsx
+++ app/imports/ui/pages/ListPassword.jsx
@@ -32,6 +32,7 @@ const ListPassword = () => {
                 <th>Website</th>
                 <th>Username</th>
                 <th>Password</th>
+                <th />
                 <th>Edit</th>
               </tr>
             </thead>
